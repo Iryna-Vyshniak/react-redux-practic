0f0a2d082307d10e6a487159dffa655078b4d512
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 20:39:12 +0300

    create and complete todo bar

---
+++ src/components/ToDo/ToDoBar.jsx
@@ -0,0 +1,17 @@
+import { ToDoCounter } from './ToDoCounter';
+import { FormFilterTodo } from './FormFilterTodo';
+
+export const ToDoBar = () => {
+  return (
+    <header className='flex justify-between w-full max-w-[1200px]'>
+      <section className='grid gap-1'>
+        <h2 className='text-lg text-blue-700 font-semibold'>Tasks</h2>
+        <ToDoCounter />
+      </section>
+      <section className='grid gap-1'>
+        <h2 className='text-lg text-blue-700 font-semibold'>Filter by status</h2>
+        <FormFilterTodo />
+      </section>
+    </header>
+  );
+};
