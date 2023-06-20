7ea540c16f0bcd42957e5940b6f27834d5a2622d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 20:40:40 +0300

    complete todo page, add UI

--- src/pages/ToDoPage.jsx
+++ src/pages/ToDoPage.jsx
@@ -1,5 +1,15 @@
+import { ToDoBar } from '../components/ToDo/ToDoBar';
+import { ToDoForm } from '../components/ToDo/ToDoForm';
+import { ToDoList } from '../components/ToDo/ToDoList';
+
 const ToDoPage = () => {
-  return <div>ToDo</div>;
+  return (
+    <div className='grid gap-4 mx-auto px-3 py-3 w-full'>
+      <ToDoBar />
+      <ToDoForm />
+      <ToDoList />
+    </div>
+  );
 };
 
 export default ToDoPage;
