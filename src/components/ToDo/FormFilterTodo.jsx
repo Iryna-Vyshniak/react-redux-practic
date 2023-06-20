14f98aeabcfc60f3a835eebbf429f049c54cae18
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 20:37:25 +0300

    Create form filter, comlete, add UI

---
+++ src/components/ToDo/FormFilterTodo.jsx
@@ -0,0 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from './Button';
+import { statusFilters } from '../../../store/constants';
+import { setStatusFilter } from '../../../store/filter/actions';
+import { getStatusFilter } from '../../../store/selectors';
+
+export const FormFilterTodo = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusFilter);
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+
+  return (
+    <div className='flex space-x-4'>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
