1455e703dacaf7ba518913d1e8e607a525cd2ad5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 11:28:15 +0300

    add main

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from './components/footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0);
+
+  const setCountPlus = () => {};
+  const setCountMinus = () => {};
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='flex flex-col min-h-screen'>
+      <h1 className='font-semibold text-lg text-red-700 text-center mt-6'>Vite + React</h1>
+      <main className='flex items-center justify-center flex-1'>
+        <h3>{}</h3>
+        <button
+          className='items-center mt-6 mb-6 mr-2 px-1 py-1 max-w-[150px] w-full h-[44px] bg-red-700 text-white rounded-md shadow-lg transition duration-200 ease-in-out hover:bg-red-600 hover:shadow-md shadow-emerald-50 active:scale-0'
+          onClick={setCountPlus}
+        >
+          Plus
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+        <button
+          className='items-center mt-6 mb-6 px-1 py-1 max-w-[150px] w-full h-[44px] bg-blue-700 text-white rounded-md shadow-lg transition duration-200 ease-in-out hover:bg-blue-600 hover:shadow-md shadow-emerald-50 active:scale-0'
+          onClick={setCountMinus}
+        >
+          Minus
+        </button>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
