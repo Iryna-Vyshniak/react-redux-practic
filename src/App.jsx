134c586443db16e499febac4f4604776c15415cb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 20:35:23 +0300

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import UseRoutes from './components/Layout/UseRoutes';
+import { ToastContainer, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return <UseRoutes />;
+  return (
+    <>
+      <UseRoutes />
+      <ToastContainer transition={Slide} draggablePercent={60} />
+    </>
+  );
 }
 
 export default App;
