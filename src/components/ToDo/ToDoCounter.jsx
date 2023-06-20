c8a1f68130907a2fb8b584a04139784cc177a09a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 20:38:49 +0300

    create and complete todo counter

---
+++ src/components/ToDo/ToDoCounter.jsx
@@ -0,0 +1,25 @@
+import { useSelector } from 'react-redux';
+import { getToDo } from '../../../store/selectors';
+
+export const ToDoCounter = () => {
+  const todo = useSelector(getToDo);
+
+  const count = todo.reduce(
+    (acc, task) => {
+      if (task.completed) {
+        acc.completed += 1;
+      } else {
+        acc.active += 1;
+      }
+      return acc;
+    },
+    { active: 0, completed: 0 }
+  );
+
+  return (
+    <div>
+      <p className='m-0'>Active: {count.active}</p>
+      <p className='m-0'>Completed: {count.completed}</p>
+    </div>
+  );
+};
