c444bb1a755a6040a65ae76fa94c87616c6368fb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-06-20 14:06:05 +0300

    create header, add UI

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,49 @@
+import { Link, NavLink } from 'react-router-dom';
+import { SiDeepnote } from 'react-icons/si';
+
+const Header = () => {
+  const activeLink =
+    'cursor-pointer py-3 text-sm font-semibold text-black-700 border-b-[3px] border-b-blue-700';
+
+  return (
+    <div className='sticky top-0 z-40 bg-white border-b shadow-sm'>
+      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+        <div>
+          <Link to='/'>
+            <SiDeepnote className='h-5 cursor-pointer text-red-700 hover:text-blue-700' />
+          </Link>
+        </div>
+        <nav>
+          <ul className='flex space-x-10'>
+            <li className='py-3'>
+              <NavLink
+                to='/'
+                className={({ isActive }) =>
+                  isActive
+                    ? activeLink
+                    : 'cursor-pointer px-3 text-sm font-semibold text-slate-400 border-b-[3px] border-b-transparent'
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className='py-3'>
+              <NavLink
+                to='/counter'
+                className={({ isActive }) =>
+                  isActive
+                    ? activeLink
+                    : 'cursor-pointer px-3 text-sm font-semibold text-slate-600 border-b-[3px] border-b-transparent'
+                }
+              >
+                Counter
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
