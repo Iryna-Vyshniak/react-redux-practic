efc5bd593c8cb1c32e72d1c46f4ce7c3d6e1daa2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-07 10:47:20 +0300

    refactor: use createAction

--- store/counter/actions.js
+++ store/counter/actions.js
@@ -1,7 +1,12 @@
+import { createAction } from '@reduxjs/toolkit';
+
 import { DECREMENT, INCREMENT, SETSTEP } from './types';
 
-export const increment = (step) => ({ type: INCREMENT, payload: step });
-export const decrement = (step) => ({ type: DECREMENT, payload: step });
-export const setStep = (value) => ({ type: SETSTEP, payload: value });
-// export const increment = () => ({ type: 'increment' });
-// export const decrement = () => ({ type: 'decrement' });
+// export const increment = (step) => ({ type: INCREMENT, payload: step });
+// export const decrement = (step) => ({ type: DECREMENT, payload: step });
+// export const setStep = (value) => ({ type: SETSTEP, payload: value });
+
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+export const setStep = createAction(SETSTEP);
+// createAction() => { type: example, payload: example }
