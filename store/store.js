dc7da6c960b440b018034b60be1d65504f692559
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-07 10:08:09 +0300

    refactor: use configureStore

--- store/store.js
+++ store/store.js
@@ -1,51 +1,14 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './reducer';
+// import { reducer as rootReducer } from './reducer';
 
-export const store = createStore(reducer);
+export const store = configureStore({ reducer });
+// export const store = configureStore({ reducer: reducer})
+// export const store = configureStore({ reducer: rootReducer})
 
-// ----------------- 2nd opt variant --------------------------
-// const initialState = {
-//   counter: { total: 0, step: 1 },
-//   todo: { todo: [] },
-// };
+// pure redux
 
-//export const store = createStore(reducer, initialState);
-// export const store = createStore(reducer, { counter: { total: 0, step: 1 }, todo: { todo: [] } });
+// import { createStore } from 'redux';
+// import { reducer } from './reducer';
 
-// ----------------- 1st variant --------------------------
-
-// const reducer = (state, action) => {
-//   switch (action.type) {
-//     case 'increment':
-//       return {
-//         ...state,
-//         total: state.total + action.payload,
-//       };
-//     case 'decrement':
-//       return {
-//         ...state,
-//         total: state.total - action.payload,
-//       };
-//     case 'setStep':
-//       return {
-//         ...state,
-//         step: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export const store = createStore(reducer, { total: 0, step: 1 });
-
-//console.log(store.getState()); // {total: 0} => useSelector(state => state.total)
-// store.dispatch({type: 'increment', payload: 1}); => useDispatch(action)
-/* 
-store:
-Object
-@@observable: ƒ observable()
-dispatch: ƒ dispatch(action) // => setState
-getState: ƒ getState() // => state
-replaceReducer: ƒ replaceReducer(nextReducer)
-subscribe: ƒ subscribe(listener)
-[[Prototype]]: Object */
+// export const store = createStore(reducer);
