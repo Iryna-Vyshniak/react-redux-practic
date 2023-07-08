2f1e055061bda0303fc7e782ec361007403188db
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 21:54:28 +0300

    refactor: use createAction

--- store/todo/actions.js
+++ store/todo/actions.js
@@ -1,27 +1,54 @@
-import { nanoid } from 'nanoid';
-import { ADDTASK, DELETETASK, TOGGLECOMPLETED } from './types';
+import { createAction, nanoid } from '@reduxjs/toolkit';
 
-export const addTask = (text) => {
+export const addTask = createAction('ADDTASK', (text) => {
   return {
-    type: ADDTASK,
     payload: {
       id: nanoid(),
       completed: false,
       text,
     },
   };
-};
+});
 
-export const deleteTask = (taskId) => {
-  return {
-    type: DELETETASK,
-    payload: taskId,
-  };
-};
+export const deleteTask = createAction('DELETETASK');
+// export const deleteTask = createAction('DELETETASK', taskId => {
+//   return {
+//     payload: taskId,
+//   }
+// }
+// );
 
-export const toggleCompleted = (taskId) => {
-  return {
-    type: TOGGLECOMPLETED,
-    payload: taskId,
-  };
-};
+export const toggleCompleted = createAction('TOGGLECOMPLETED');
+// export const toggleCompleted = createAction('TOGGLECOMPLETED', (taskId) => {
+//   return {
+//     payload: taskId,
+//   }
+// }
+// )
+
+//  pure redux
+
+// export const addTask = (text) => {
+//   return {
+//     type: ADDTASK,
+//     payload: {
+//       id: nanoid(),
+//       completed: false,
+//       text,
+//     },
+//   };
+// };
+
+// export const deleteTask = (taskId) => {
+//   return {
+//     type: DELETETASK,
+//     payload: taskId,
+//   };
+// };
+
+// export const toggleCompleted = (taskId) => {
+//   return {
+//     type: TOGGLECOMPLETED,
+//     payload: taskId,
+//   };
+// };
