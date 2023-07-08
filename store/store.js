6deabb1455b6e78bf1247ca263aa7611a753660b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 23:02:04 +0300

    customized the middleware by configuring it to ignore specific action types, or fields in actions and state

--- store/store.js
+++ store/store.js
@@ -1,5 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { reducer } from './reducer';
 // import { reducer as rootReducer } from './reducer';
@@ -11,7 +20,15 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-export const store = configureStore({ reducer: persistedReducer });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 // export const store = configureStore({ reducer: reducer})
 // export const store = configureStore({ reducer: rootReducer})
 
