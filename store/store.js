1afd64679a40d09bf61c3f0d8246c823fa47b961
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 22:52:44 +0300

    add  persistStore, persistReducer, storage

--- store/store.js
+++ store/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { reducer } from './reducer';
 // import { reducer as rootReducer } from './reducer';
 
-export const store = configureStore({ reducer });
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({ reducer: persistedReducer });
 // export const store = configureStore({ reducer: reducer})
 // export const store = configureStore({ reducer: rootReducer})
+
+export const persistor = persistStore(store);
 
 // pure redux
 
