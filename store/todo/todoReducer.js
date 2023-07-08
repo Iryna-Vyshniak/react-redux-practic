dfb324e2abe359570d53d1afaa6722a5541b9461
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 21:54:59 +0300

    refactor: use createReducer

--- store/todo/todoReducer.js
+++ store/todo/todoReducer.js
@@ -1,20 +1,35 @@
 import { todoInitialState } from './initialState';
-import { ADDTASK, DELETETASK, TOGGLECOMPLETED } from './types';
+import { addTask, deleteTask, toggleCompleted } from './actions';
+import { createReducer } from '@reduxjs/toolkit';
 
-export const todoReducer = (state = todoInitialState, action) => {
-  switch (action.type) {
-    case ADDTASK:
-      return [...state, action.payload];
-    case DELETETASK:
-      return state.filter((task) => task.id !== action.payload);
-    case TOGGLECOMPLETED:
-      return state.map((task) => {
-        if (task.id !== action.payload) {
-          return task;
-        }
-        return { ...task, completed: !task.completed };
-      });
-    default:
-      return state;
-  }
-};
+export const todoReducer = createReducer(todoInitialState, {
+  [addTask]: (state, action) => [...state, action.payload],
+  [deleteTask]: (state, action) => state.filter((todo) => todo.id !== action.payload),
+  // [deleteTask]: (state, action) => {
+  // const index = state.findIndex(task => task.id === action.payload);
+  //   state.splice(index, 1)
+  // },
+
+  [toggleCompleted]: (state, action) =>
+    state.map((todo) =>
+      todo.id !== action.payload ? todo : { ...todo, completed: !todo.completed }
+    ),
+});
+
+// export const todoReducer = (state = todoInitialState, action) => {
+//   switch (action.type) {
+//     case ADDTASK:
+//       return [...state, action.payload];
+//     case DELETETASK:
+//       return state.filter((task) => task.id !== action.payload);
+//     case TOGGLECOMPLETED:
+//       return state.map((task) => {
+//         if (task.id !== action.payload) {
+//           return task;
+//         }
+//         return { ...task, completed: !task.completed };
+//       });
+//     default:
+//       return state;
+//   }
+// };
