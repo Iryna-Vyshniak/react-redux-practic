0e4a80095c34159c9c37bf0a535c7faf14178d93
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 21:54:00 +0300

    refactor: use createReducer

--- store/filter/filterReducer.js
+++ store/filter/filterReducer.js
@@ -1,14 +1,20 @@
 import { filtersInitialState } from './initialState';
-import { SET_STATUS_FILTER } from './types';
+import { setStatusFilter } from './actions';
+import { createReducer } from '@reduxjs/toolkit';
 
-export const filtersReducer = (state = filtersInitialState, action) => {
-  switch (action.type) {
-    case SET_STATUS_FILTER:
-      return {
-        ...state,
-        status: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export const filtersReducer = createReducer(filtersInitialState, {
+  [setStatusFilter]: (state, action) => {
+    state.status = action.payload;
+  },
+});
+// export const filtersReducer = (state = filtersInitialState, action) => {
+//   switch (action.type) {
+//     case SET_STATUS_FILTER:
+//       return {
+//         ...state,
+//         status: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
