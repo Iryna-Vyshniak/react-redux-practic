799890e6e409c120ded1b1f5be5a2521304b5816
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-08 21:52:49 +0300

    refactor: use create reducer

--- store/counter/counterReducer.js
+++ store/counter/counterReducer.js
@@ -1,26 +1,43 @@
-import { DECREMENT, INCREMENT, SETSTEP } from './types';
+import { createReducer } from '@reduxjs/toolkit';
+import { increment, decrement, setStep } from './actions';
 import { counterInitialState } from './initialState';
 
-export const counterReducer = (state = counterInitialState, action) => {
-  switch (action.type) {
-    case INCREMENT:
-      return {
-        ...state,
-        total: state.total + action.payload,
-      };
-    case DECREMENT:
-      return {
-        ...state,
-        //   or
-        //total: state.total - state.step,
-        total: state.total - action.payload,
-      };
-    case SETSTEP:
-      return {
-        ...state,
-        step: action.payload,
-      };
-    default:
-      return state;
-  }
-};
+export const counterReducer = createReducer(counterInitialState, {
+  // [increment] => викликаємо функцію в [], без [] - ключ-назва
+  [increment]: (state, action) => ({
+    ...state,
+    total: state.total + action.payload,
+  }),
+  [decrement]: (state, action) => ({
+    ...state,
+    total: state.total - action.payload,
+  }),
+  [setStep]: (state, action) => ({
+    ...state,
+    step: action.payload,
+  }),
+});
+
+// export const counterReducer = (state = counterInitialState, action) => {
+//   switch (action.type) {
+//     case INCREMENT:
+//       return {
+//         ...state,
+//         total: state.total + action.payload,
+//       };
+//     case DECREMENT:
+//       return {
+//         ...state,
+//         //   or
+//         //total: state.total - state.step,
+//         total: state.total - action.payload,
+//       };
+//     case SETSTEP:
+//       return {
+//         ...state,
+//         step: action.payload,
+//       };
+//     default:
+//       return state;
+//   }
+// };
