91d7a6e85138821e2a662e5e2a7264775b8c3fc5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-10 21:37:27 +0300

    complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,28 @@
+import { useSearchParams } from 'react-router-dom';
+import { getProducts } from '../../share/api/productsAPI.JS';
+import { SearchBox } from '../components/Products/SearchBox';
+import { ProductsList } from '../components/Products/ProductsList';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get('name') ?? '';
+
+  const visibleProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQueryString = (name) => {
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+  };
+
+  return (
+    <main>
+      <SearchBox value={productName} onChange={updateQueryString} />
+      <ProductsList products={visibleProducts} />
+    </main>
+  );
 };
 
 export default HomePage;
