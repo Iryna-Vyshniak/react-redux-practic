b971d11f313714e2b437814443f04a4945c41b57
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-11 22:12:41 +0300

    update UI

--- src/components/Products/SearchBox.jsx
+++ src/components/Products/SearchBox.jsx
@@ -2,14 +2,16 @@ import { HiSearch } from 'react-icons/hi';
 
 export const SearchBox = ({ value, onChange }) => {
   return (
-    <div className='relative inline-flex items-center mb-4 mt-4 uppercase'>
-      <HiSearch className='absolute right-1 w-5 h-5' />
-      <input
-        className='px-2 py-2 pr-8 rounded font-inherit'
-        type='text'
-        value={value}
-        onChange={(e) => onChange(e.target.value)}
-      />
+    <div className='flex items-center justify-center mb-4 mt-4 uppercase'>
+      <div className='relative inline-flex items-center justify-center mb-4 mt-4 uppercase'>
+        <HiSearch className='absolute right-1 w-5 h-5 text-red-700' />
+        <input
+          className='px-2 py-2 pr-8 rounded font-inherit shadow-lg shadow-red-100'
+          type='text'
+          value={value}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      </div>
     </div>
   );
 };
