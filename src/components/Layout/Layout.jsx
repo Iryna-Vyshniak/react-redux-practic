24b1a2bc99837058a196deb701293cf2f56c406d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-11 22:17:53 +0300

    update UI

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = () => {
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
-      <main className='flex flex-col items-center  flex-1'>
+      <main className='flex flex-col items-center flex-1 p-4'>
         <Suspense fallback={<h3>Loading...</h3>}>
           <Outlet />
         </Suspense>
