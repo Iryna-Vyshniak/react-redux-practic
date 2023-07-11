a4c0b4dd5d378e9a4ce91b40ec7d9892193fb7f6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-11 22:15:05 +0300

    create and complete register form

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,75 @@
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
+import { useState } from 'react';
+
+const RegisterForm = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch();
+    form.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete='off'>
+      <label className='flex flex-col mb-4'>
+        Name
+        <input
+          type='text'
+          name='name'
+          className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+        />
+      </label>
+      <label className='flex flex-col mb-4'>
+        Email
+        <input
+          type='email'
+          name='email'
+          className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+        />
+      </label>
+      <label className='flex flex-col mb-4'>
+        Password
+        <input
+          type={showPassword ? 'text' : 'password'}
+          name='password'
+          className='mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out'
+        />
+        {showPassword ? (
+          <AiFillEyeInvisible
+            className='absolute right-3 top-3 text-xl cursor-pointer'
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        ) : (
+          <AiFillEye
+            className='absolute right-3 top-3 text-xl cursor-pointer'
+            onClick={() => setShowPassword((prevState) => !prevState)}
+          />
+        )}
+      </label>
+      <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg'>
+        <p className='mb-6'>
+          Have an account?
+          <Link
+            to='/signin'
+            className='ml-1 text-red-700 hover:text-red-600 transition-duration-200 ease-in-out'
+          >
+            Sign in
+          </Link>
+        </p>
+      </div>
+      <button
+        type='submit'
+        className='w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-800'
+      >
+        Register
+      </button>
+    </form>
+  );
+};
+
+export default RegisterForm;
