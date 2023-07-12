37eb234de2511d823377114827abc6a152070e2a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-13 00:32:54 +0300

    create and complete navbar

---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,19 @@
+import { useSelector } from 'react-redux';
+import { isUserLogin } from '../../../store/auth/selectors';
+import { AuthNav } from './AuthNav';
+import { UserNav } from './UserNav';
+import NavbarMenu from './NavbarMenu';
+
+const Navbar = () => {
+  const isLogin = useSelector(isUserLogin);
+
+  return (
+    <div className='flex justify-between items-center'>
+      <NavbarMenu />
+      {!isLogin && <AuthNav />}
+      {isLogin && <UserNav />}
+    </div>
+  );
+};
+
+export default Navbar;
