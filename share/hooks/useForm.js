effc96ba54b330c40491340e322a66c782f28494
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-12 23:28:13 +0300

    create hook useForm

---
+++ share/hooks/useForm.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react';
+
+const useForm = ({ initialState, onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
+
+  const handleChange = useCallback(
+    (name, value) => {
+      setState((prevState) => ({ ...prevState, [name]: value }));
+    },
+    [setState]
+  );
+
+  const reset = useCallback(() => setState({ ...initialState }), [initialState]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({ ...state });
+    reset();
+  };
+
+  return { state, setState, handleChange, handleSubmit, reset };
+};
+
+export default useForm;
