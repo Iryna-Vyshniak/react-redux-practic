cf384a5e11950fb379aba654704f22cb9a491015
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-13 00:32:22 +0300

    create and complete navbar menu

---
+++ src/components/Layout/NavbarMenu.jsx
@@ -0,0 +1,34 @@
+import { useSelector } from 'react-redux';
+import { isUserLogin } from '../../../store/auth/selectors';
+import items from '../../../share/menu-items.json';
+import { NavLink } from 'react-router-dom';
+
+const NavbarMenu = () => {
+  const isLogin = useSelector(isUserLogin);
+
+  const activeLink =
+    'cursor-pointer py-3 text-sm font-semibold text-black-700 border-b-[3px] border-b-blue-700 transition duration-200 ease-in-out';
+
+  const nav = !isLogin ? items.filter((item) => !item.private) : items;
+
+  const li = nav.map(({ id, link, text }) => {
+    return (
+      <li key={id} className='py-3'>
+        <NavLink
+          to={link}
+          className={({ isActive }) =>
+            isActive
+              ? activeLink
+              : 'cursor-pointer px-3 text-sm font-semibold text-slate-400 border-b-[3px] border-b-transparent'
+          }
+        >
+          {text}
+        </NavLink>
+      </li>
+    );
+  });
+
+  return <ul className='flex space-x-10'>{li}</ul>;
+};
+
+export default NavbarMenu;
