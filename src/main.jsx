b53f004d658aae6d836b92b465bd5ac486c8fa93
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-14 00:18:38 +0300

    add auth layout

--- src/main.jsx
+++ src/main.jsx
@@ -7,15 +7,18 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { persistor, store } from '../store/store.js';
+import AuthLayout from './components/Layout/AuthLayout.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <AuthLayout>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </AuthLayout>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
