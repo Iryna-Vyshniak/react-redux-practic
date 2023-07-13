44c8f056caa00a187abb204acc3de44284535be7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-14 00:16:29 +0300

    add navigate to login

--- src/components/Layout/PrivateRoute.jsx
+++ src/components/Layout/PrivateRoute.jsx
@@ -1,17 +1,16 @@
 import { useSelector } from 'react-redux';
 import { getAuth } from '../../../store/auth/selectors';
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 
 const PrivateRoute = () => {
   const { isLogin, token } = useSelector(getAuth);
-  const navigate = useNavigate();
 
   if (!isLogin && token) {
     return <p>Loading...</p>;
   }
 
   if (!isLogin && !token) {
-    return navigate('/signin');
+    return <Navigate to='/signin' />;
   }
 
   return <Outlet />;
