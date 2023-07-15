5f848246fbce170c6ae1eb7c90c3c733b6c6c828
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-15 04:38:24 +0300

    add and complete instance

--- share/api/productsAPI.js
+++ share/api/productsAPI.js
@@ -1,96 +1,130 @@
-// https://simple-products-backend.onrender.com/
-// https://simple-products-backend.onrender.com/api/products
-const products = [
-  {
-    id: 1,
-    name: 'Brown eggs',
-    type: 'dairy',
-    description: 'Raw organic brown eggs in a basket',
-    url: 'https://atlas-content-cdn.pixelsquid.com/stock-images/bowl-of-brown-eggs-egg-KaB5Wz6-600.jpg',
-    price: 28.1,
-  },
-  {
-    id: 2,
-    name: 'Sweet fresh stawberry',
-    type: 'fruit',
-    description: 'Sweet fresh stawberry on the wooden table',
-    url: 'https://pngimg.com/uploads/strawberry/strawberry_PNG2598.png',
-    price: 29.45,
-  },
-  {
-    id: 3,
-    name: 'Asparagus',
-    type: 'vegetable',
-    description: 'Asparagus with ham on the wooden table',
-    url: 'https://pngimg.com/uploads/asparagus/asparagus_PNG12.png',
-    price: 18.95,
-  },
-  {
-    id: 4,
-    name: 'Green smoothie',
-    type: 'dairy',
-    description:
-      "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
-    url: 'https://img.freepik.com/free-photo/green-detox-smoothie-smoothie-recipes-fast-weight-loss_144627-30065.jpg?w=1060&t=st=1689009883~exp=1689010483~hmac=9459d57483ac06a09ef864fc96094c01c6a54f3ab776418292890fb24ed8647d',
-    price: 17.68,
-  },
-  {
-    id: 5,
-    name: 'Raw legums',
-    type: 'vegetable',
-    description: 'Raw legums on the wooden table',
-    url: 'https://www.rosannadavisonnutrition.com/wp-content/uploads/2015/04/legumes.jpg',
-    price: 17.11,
-  },
-  {
-    id: 6,
-    name: 'Baking cake',
-    type: 'dairy',
-    description:
-      'Baking cake in rural kitchen - dough  recipe ingredients (eggs, flour, sugar) on vintage wooden table from above.',
-    url: 'https://www.nicepng.com/png/detail/32-322780_carrot-cake-png-cake.png',
-    price: 11.14,
-  },
-  {
-    id: 7,
-    name: 'Pesto with basil',
-    type: 'vegetable',
-    description: 'Italian traditional pesto with basil, chesse and oil',
-    url: 'https://www.innit.com/meal-service/en-GB/images/Meal-Meal-Pasta%3A%20No-Protein%2BRoasted-Broccoli%2BBlended-Pesto_Sauce%2BBoiled-Spaghetti_960x960.png',
-    price: 18.19,
-  },
-  {
-    id: 8,
-    name: 'Hazelnut in black ceramic bowl',
-    type: 'vegetable',
-    description:
-      'Hazelnut in black ceramic bowl on old wooden background. forest wealth. rustic style. selective focus',
-    url: 'https://img.freepik.com/premium-photo/mixed-nuts-white-wooden-bowl-white-background-macadamia-almonds-hazelnuts-cashew_615349-261.jpg',
-    price: 27.35,
-  },
-  {
-    id: 9,
-    name: 'Fresh stawberry',
-    type: 'fruit',
-    description: 'Sweet fresh stawberry on the wooden table',
-    url: 'https://purepng.com/public/uploads/large/bowl-full-of-strawberries-04s.png',
-    price: 28.59,
-  },
-  {
-    id: 10,
-    name: 'Lemon and salt',
-    type: 'fruit',
-    description: 'Rosemary, lemon and salt on the table',
-    url: 'https://pngimg.com/uploads/lemon/lemon_PNG25196.png',
-    price: 15.79,
-  },
-];
+import axios from 'axios';
 
-export const getProducts = () => {
-  return products;
+const instance = axios.create({
+  baseURL: 'https://simple-products-backend.onrender.com/api',
+});
+
+export const getAllProducts = async () => {
+  try {
+    const data = await instance.get('/products');
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-export const getProductById = (productId) => {
-  const id = parseInt(productId, 10);
-  return products.find((product) => product.id === id);
+export const createProduct = async (body) => {
+  try {
+    const data = await instance.post('/products', body);
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
 };
+
+export const getProductById = async (id) => {
+  try {
+    const data = await instance.get(`/products/${id}`);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export default instance;
+
+// const products = [
+//   {
+//     id: 1,
+//     name: 'Brown eggs',
+//     type: 'dairy',
+//     description: 'Raw organic brown eggs in a basket',
+//     url: 'https://atlas-content-cdn.pixelsquid.com/stock-images/bowl-of-brown-eggs-egg-KaB5Wz6-600.jpg',
+//     price: 28.1,
+//   },
+//   {
+//     id: 2,
+//     name: 'Sweet fresh stawberry',
+//     type: 'fruit',
+//     description: 'Sweet fresh stawberry on the wooden table',
+//     url: 'https://pngimg.com/uploads/strawberry/strawberry_PNG2598.png',
+//     price: 29.45,
+//   },
+//   {
+//     id: 3,
+//     name: 'Asparagus',
+//     type: 'vegetable',
+//     description: 'Asparagus with ham on the wooden table',
+//     url: 'https://pngimg.com/uploads/asparagus/asparagus_PNG12.png',
+//     price: 18.95,
+//   },
+//   {
+//     id: 4,
+//     name: 'Green smoothie',
+//     type: 'dairy',
+//     description:
+//       "Glass of green smoothie with quail egg's yolk, served with cocktail tube, green apple and baby spinach leaves over tin surface.",
+//     url: 'https://img.freepik.com/free-photo/green-detox-smoothie-smoothie-recipes-fast-weight-loss_144627-30065.jpg?w=1060&t=st=1689009883~exp=1689010483~hmac=9459d57483ac06a09ef864fc96094c01c6a54f3ab776418292890fb24ed8647d',
+//     price: 17.68,
+//   },
+//   {
+//     id: 5,
+//     name: 'Raw legums',
+//     type: 'vegetable',
+//     description: 'Raw legums on the wooden table',
+//     url: 'https://www.rosannadavisonnutrition.com/wp-content/uploads/2015/04/legumes.jpg',
+//     price: 17.11,
+//   },
+//   {
+//     id: 6,
+//     name: 'Baking cake',
+//     type: 'dairy',
+//     description:
+//       'Baking cake in rural kitchen - dough  recipe ingredients (eggs, flour, sugar) on vintage wooden table from above.',
+//     url: 'https://www.nicepng.com/png/detail/32-322780_carrot-cake-png-cake.png',
+//     price: 11.14,
+//   },
+//   {
+//     id: 7,
+//     name: 'Pesto with basil',
+//     type: 'vegetable',
+//     description: 'Italian traditional pesto with basil, chesse and oil',
+//     url: 'https://www.innit.com/meal-service/en-GB/images/Meal-Meal-Pasta%3A%20No-Protein%2BRoasted-Broccoli%2BBlended-Pesto_Sauce%2BBoiled-Spaghetti_960x960.png',
+//     price: 18.19,
+//   },
+//   {
+//     id: 8,
+//     name: 'Hazelnut in black ceramic bowl',
+//     type: 'vegetable',
+//     description:
+//       'Hazelnut in black ceramic bowl on old wooden background. forest wealth. rustic style. selective focus',
+//     url: 'https://img.freepik.com/premium-photo/mixed-nuts-white-wooden-bowl-white-background-macadamia-almonds-hazelnuts-cashew_615349-261.jpg',
+//     price: 27.35,
+//   },
+//   {
+//     id: 9,
+//     name: 'Fresh stawberry',
+//     type: 'fruit',
+//     description: 'Sweet fresh stawberry on the wooden table',
+//     url: 'https://purepng.com/public/uploads/large/bowl-full-of-strawberries-04s.png',
+//     price: 28.59,
+//   },
+//   {
+//     id: 10,
+//     name: 'Lemon and salt',
+//     type: 'fruit',
+//     description: 'Rosemary, lemon and salt on the table',
+//     url: 'https://pngimg.com/uploads/lemon/lemon_PNG25196.png',
+//     price: 15.79,
+//   },
+// ];
+
+// export const getProducts = () => {
+//   return products;
+// };
+
+// export const getProductById = (productId) => {
+//   const id = parseInt(productId, 10);
+//   return products.find((product) => product.id === id);
+// };
