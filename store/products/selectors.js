57476a4ac36ba101db7f9da0e1b04bbc3dcdfd39
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-18 22:29:14 +0300

    create selectProductDetails

--- store/products/selectors.js
+++ store/products/selectors.js
@@ -2,6 +2,7 @@ export const selectIsLoading = (state) => state.products.isLoading;
 export const selectError = (state) => state.products.error;
 
 export const selectProducts = (state) => state.products.items;
+export const selectProductDetails = (state) => state.products.productDetails;
 
 export const selectFavoriteProduct = (state) =>
   state.products.items.selectFavoriteProductfilter(({ favorite }) => favorite);
