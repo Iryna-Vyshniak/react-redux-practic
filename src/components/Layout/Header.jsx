36e2281f732e6cd89ef8f4c441add444105f246c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-20 21:52:53 +0300

    update UI

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -9,20 +9,18 @@ const Header = () => {
   const isLogin = useSelector(isUserLogin);
 
   return (
-    <div className='sticky top-0 z-40 bg-white border-b shadow-sm'>
-      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-        <div>
-          <Link to='/' className='flex flex-col items-center justify-center'>
-            <SiDeepnote className='h-5 cursor-pointer text-blue-700 hover:text-red-700' />
-            <span className='mr-2 text-blue-700 font-semibold'>WorkShopDev</span>{' '}
-          </Link>
-        </div>
-        <nav className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-          <Navbar />
-        </nav>
-        {isLogin && <UserNav />}
-      </header>
-    </div>
+    <header className='sticky top-0 z-40 flex justify-between items-center px-3  mx-auto max-w-6xl w-full bg-white border-b shadow-sm'>
+      <div>
+        <Link to='/' className='flex flex-col items-center justify-center'>
+          <SiDeepnote className='h-5 cursor-pointer text-blue-700 hover:text-red-700' />
+          <span className='mr-2 text-blue-700 font-semibold'>WorkShopDev</span>{' '}
+        </Link>
+      </div>
+      <nav className='flex justify-between items-center px-3 mx-auto'>
+        <Navbar />
+      </nav>
+      {isLogin && <UserNav />}
+    </header>
   );
 };
 
