743a64270f4259ae85fa70595105e41cd5a1f300
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-21 01:51:23 +0300

    update ui

--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -5,9 +5,9 @@ export const BackLink = ({ to, children }) => {
   return (
     <Link
       to={to}
-      className='inline-flex items-center py-2 px-1 text-blue-700 uppercase font-medium rounded-md shadow-lg transition duration-200 ease-in-out hover:bg-blue-600 hover:text-white hover:shadow-md shadow-emerald-50'
+      className='inline-flex items-center mb-4 py-2 px-1 text-blue-700 uppercase text-xs font-medium rounded-md shadow-lg transition duration-200 ease-in-out hover:bg-sky-50 hover:text-blue-400 hover:drop-shadow-md shadow-emerald-50'
     >
-      <HiArrowLeft size='24' />
+      <HiArrowLeft className='w-4 h-4 space-x-3' />
       {children}
     </Link>
   );
