187ffdd367faebbb220cc76e779014bda0321360
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-20 21:51:20 +0300

    get thunks

--- src/components/Products/ProductsList.jsx
+++ src/components/Products/ProductsList.jsx
@@ -1,25 +1,54 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
+import { getAllProductsThunk } from '../../../store/products/thunks';
 
 export const ProductsList = ({ products }) => {
   const location = useLocation();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    try {
+      dispatch(getAllProductsThunk());
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, [dispatch]);
 
   return (
     <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
       {products?.map((product) => (
         <li
           key={product._id}
-          className='relative bg-white flex flex-col justify-between items-center m-[10px] p-4 shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-200 ease-in-out'
+          className='flex flex-col justify-between items-center m-[10px] p-4 bg-white shadow-md hover:shadow-xl rounded-md overflow-hidden transition-shadow duration-200 ease-in-out'
         >
-          <Link to={`${product._id}`} state={{ from: location }}>
+          <Link to={`${product._id}`} state={{ from: location }} className='contents'>
             {product.poster && (
               <img
                 src={`http://localhost:3500/${product.poster}`}
                 alt={product.name}
-                className='h-[170px] w-full object-cover hover:scale-105 transition-scale duration-200 ease-in-out rounded-md'
+                className='h-[170px] w-full object-contain hover:scale-105 transition-scale duration-200 ease-in-out rounded-md'
                 loading='lazy'
               />
             )}
-            <h3 className='p-1 mt-2 mb-0 text-black'>{product.name}</h3>
+            <div className='w-full'>
+              <h3 className='mt-2 text-black'>{product.name}</h3>
+              <div className='flex space-x-2 items-center justify-between'>
+                <p className='text-blue-600 text-xs'>{product.createdAt.split('T')[0]}</p>
+                <div className='flex space-x-2 items-center'>
+                  {product.owner.avatarUrl && (
+                    <img
+                      src={`http://localhost:3500/${product.owner.avatarUrl}`}
+                      alt={product.owner.name}
+                      className='h-5 w-5 object-cover hover:scale-105 transition-scale duration-200 ease-in-out rounded-full'
+                      loading='lazy'
+                    />
+                  )}
+                  <p className='text-blue-600 text-xs uppercase'>{product.owner.name}</p>
+                </div>
+              </div>
+            </div>
           </Link>
         </li>
       ))}
