6fbb97df6b31a83e7721e6bad867521236df80bc
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-22 20:36:08 +0300

    update path and complete UI

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 import { SiDeepnote } from 'react-icons/si';
 import Navbar from './Navbar';
-import { UserNav } from './UserNav';
+import { UserNav } from './User/UserNav';
 import { useSelector } from 'react-redux';
 import { isUserLogin } from '../../store/auth/selectors';
 
@@ -9,17 +9,19 @@ const Header = () => {
   const isLogin = useSelector(isUserLogin);
 
   return (
-    <header className='sticky top-0 z-40 flex justify-between items-center px-3  mx-auto max-w-6xl w-full bg-white border-b shadow-sm'>
-      <div>
-        <Link to='/' className='flex flex-col items-center justify-center'>
-          <SiDeepnote className='h-5 cursor-pointer text-blue-700 hover:text-red-700' />
-          <span className='mr-2 text-blue-700 font-semibold'>WorkShopDev</span>{' '}
-        </Link>
+    <header className='sticky top-0 z-40 w-full bg-white border-b shadow-sm'>
+      <div className='flex justify-between items-center px-3  mx-auto max-w-6xl'>
+        <div>
+          <Link to='/' className='flex flex-col items-center justify-center'>
+            <SiDeepnote className='h-5 cursor-pointer text-blue-700 hover:text-red-700' />
+            <span className='mr-2 text-blue-700 font-semibold'>WorkShopDev</span>{' '}
+          </Link>
+        </div>
+        <nav className='flex justify-between items-center px-3 mx-auto'>
+          <Navbar />
+        </nav>
+        {isLogin && <UserNav />}
       </div>
-      <nav className='flex justify-between items-center px-3 mx-auto'>
-        <Navbar />
-      </nav>
-      {isLogin && <UserNav />}
     </header>
   );
 };
