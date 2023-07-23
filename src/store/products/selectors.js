a96341d0126c190edb3514d3f3a9cb36af7dcb4b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-23 18:13:18 +0300

    add selectors for current page and total pages

--- src/store/products/selectors.js
+++ src/store/products/selectors.js
@@ -3,6 +3,8 @@ export const selectError = (state) => state.products.error;
 
 export const selectProducts = (state) => state.products.items;
 export const selectProductDetails = (state) => state.products.productDetails;
+export const selectCurrentPage = (state) => state.products.currentPage;
+export const selectTotalPages = (state) => state.products.totalPages;
 
 export const selectFavoriteProduct = (state) => {
   return state.items.filter(({ favorite }) => favorite);
