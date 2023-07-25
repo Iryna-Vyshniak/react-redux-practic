e40736b272008be5d437256e2662ec335cf582fa
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-25 22:57:54 +0300

    remove block div

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,18 +1,16 @@
 import { useSelector } from 'react-redux';
 import { isUserLogin } from '../../store/auth/selectors';
 import { AuthNav } from './AuthNav';
-// import { UserNav } from './UserNav';
 import NavbarMenu from './NavbarMenu';
 
 const Navbar = () => {
   const isLogin = useSelector(isUserLogin);
 
   return (
-    <div className='flex justify-between items-center'>
+    <>
       <NavbarMenu />
       {!isLogin && <AuthNav />}
-      {/* {isLogin && <UserNav />} */}
-    </div>
+    </>
   );
 };
 
