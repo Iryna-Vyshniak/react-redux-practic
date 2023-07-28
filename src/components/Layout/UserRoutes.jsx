6474a10148cb467792d1d5675751185e759ae5b5
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-28 22:47:35 +0300

    add posts page route

--- src/components/Layout/UserRoutes.jsx
+++ src/components/Layout/UserRoutes.jsx
@@ -12,6 +12,7 @@ import RegisterPage from '../../pages/RegisterPage';
 import { LoginPage } from '../../pages/LoginPage';
 import PrivateRoute from './PrivateRoute';
 import PublicRoute from './PublicRoute';
+import PostsPage from '../../pages/PostsPage';
 
 const UserRoutes = () => {
   return (
@@ -24,6 +25,7 @@ const UserRoutes = () => {
             <Route path='/signin' element={<LoginPage />} />
           </Route>
           <Route element={<PrivateRoute />}>
+            <Route path='/posts' element={<PostsPage />} />
             <Route path='/products' element={<ProductPage />} />
             <Route path='/products/add-product' element={<CreateProductsPage />} />
             <Route path='/products/:id' element={<ProductDetailsPage />} />
