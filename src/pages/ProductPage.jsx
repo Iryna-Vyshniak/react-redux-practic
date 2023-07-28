fa44159a4b45869437461b93b162cdbd06cdfefa
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-28 22:51:09 +0300

    add filtered products by name

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,62 +1,73 @@
-import { NavLink, Outlet, useLocation, useSearchParams } from 'react-router-dom';
+import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
 import { SearchBox } from '../components/Products/SearchBox';
-import { Suspense, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectCurrentPage, selectProducts, selectTotalPages } from '../store/products/selectors';
 import { ProductsList } from '../components/Products/ProductsList';
 import { Pagination } from '../components/Pagination';
+import { getProductsByQueryThunk } from '../store/products/thunks';
 
 const ProductPage = () => {
-  const [showForm, setShowForm] = useState();
+  // const [showForm, setShowForm] = useState();
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchParams, setSearchParams] = useSearchParams({ name: '', page: 1 });
   // console.log(useSearchParams());
   //const { path } = history.location;
 
-  const productName = searchParams.get('name') ?? '';
+  let productName = searchParams.get('name') ?? '';
   const page = Number(searchParams.get('page') || 1);
   // console.log('PAGE: ', page, typeof page);
   const currentPage = useSelector(selectCurrentPage);
   const totalPages = useSelector(selectTotalPages);
 
-  const toggleForm = () => {
-    setShowForm((prevState) => !prevState);
-  };
+  const dispatch = useDispatch();
+
+  // const toggleForm = () => {
+  //   setShowForm((prevState) => !prevState);
+  // };
 
   const location = useLocation();
-  // console.log(location);
   const products = useSelector(selectProducts);
-  // console.log(products);
 
-  const visibleProducts = products?.filter((product) =>
-    product.name.toLowerCase().includes(productName.toLowerCase())
-  );
+  useEffect(() => {
+    dispatch(getProductsByQueryThunk(productName));
+  }, [dispatch, productName]);
 
-  const updateQueryString = (name) => {
-    const nextParams = name !== '' ? { name } : {};
-    setSearchParams(nextParams);
+  useEffect(() => {
+    const visibleProducts = products?.filter((product) =>
+      product.name.toLowerCase().includes(productName.toLowerCase())
+    );
+    setFilteredProducts(visibleProducts);
+  }, [productName, products]);
+
+  const handleSearchChange = ({ target }) => {
+    const inputValue = target.value;
+    productName = inputValue;
+    setSearchParams({ page: 1, name: productName });
   };
+
+  // const updateQueryString = (name) => {
+  //   const nextParams = name !== '' ? { name } : {};
+  //   setSearchParams(nextParams);
+  // };
 
   return (
     <div className='flex flex-col items-center justify-between min-h-[85vh]'>
       <section className='flex flex-col items-center space-x-3 flex-1'>
         <div className='flex items-center space-x-3'>
-          {!showForm && (
-            // (location.pathname === '/products' ||
-            //   (location.pathname === '/products' && location.search === '?page=1')) && (
-            <SearchBox value={productName} onChange={updateQueryString} />
-          )}
-          {/* <NavLink to={{ pathname: 'add-product', state: location }} onClick={toggleForm}> */}
+          <SearchBox value={productName} onChange={handleSearchChange} />
+
+          <NavLink to={'add-product'} state={location.state}>
+            Add product
+          </NavLink>
+          {/* {!showForm && <SearchBox value={productName} onChange={handleSearchChange} />}
+
           <NavLink to={showForm ? ' ' : 'add-product'} state={location.state} onClick={toggleForm}>
             {showForm ? 'Hide form' : 'Add product'}
-          </NavLink>
+          </NavLink> */}
         </div>
 
-        {/*   {showForm && (
-          <Suspense fallback={<div>Loading...</div>}>
-            <Outlet />
-          </Suspense>
-        )} */}
-        <ProductsList products={visibleProducts} page={page} />
+        <ProductsList products={filteredProducts} page={page} />
       </section>
       {products.length > 0 && (
         <section>
