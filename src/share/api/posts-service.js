c4a4d0ba65959f385008db90f67b849b46d2fcee
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-07-31 21:45:00 +0300

    create fetch to get posts

---
+++ src/share/api/posts-service.js
@@ -0,0 +1,40 @@
+/* 
+#public 
+
+router.get('/', ctrl.getAll);
+router.get('/tags', ctrl.getAllTags);
+router.get('/tags/:tag', ctrl.getPostsByTag);
+router.get('/search', ctrl.getSearchPosts);
+router.get('/:id', isValidId, ctrl.getOne);
+
+# private
+
+router.get('/user/posts', checkAuth, ctrl.getUserPosts);
+router.post('/', checkAuth, uploadCloud.single('image'), postCreateValidation, ctrl.createPost);
+router.patch('/:id', checkAuth, isValidId, uploadCloud.single('image'), ctrl.updatePost);
+router.delete('/:id', checkAuth, isValidId, ctrl.deletePost);
+ */
+import axios from 'axios';
+
+import instance from './auth-service';
+
+const API = import.meta.env.VITE_API_KEY;
+
+const publicInstance = axios.create({
+  baseURL: `${API}/api`,
+});
+
+export const getPosts = async (page) => {
+  try {
+    const response = await publicInstance.get('/posts', {
+      params: {
+        page,
+        limit: 6,
+      },
+    });
+    console.log('RESPONSE', response);
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
