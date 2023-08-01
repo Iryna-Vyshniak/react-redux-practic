215a8f42daec456ef6a18b12505eece4f49ada7a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-02 02:19:06 +0300

    add block with all tags

--- src/pages/PostsPage.jsx
+++ src/pages/PostsPage.jsx
@@ -4,13 +4,15 @@ import {
   // selectError,
   selectIsLoading,
   selectPosts,
+  selectTags,
   selectTotalPages,
 } from '../store/posts/selectors';
 import { useSearchParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { getAllPosts } from '../store/posts/thunks';
+import { getAllPosts, getAllTags } from '../store/posts/thunks';
 import { Pagination } from '../components/Pagination';
 import { PostsList } from '../components/Posts/PostsList';
+import { TagsBlock } from '../components/Posts/TagsBlock';
 
 const PostsPage = () => {
   const dispatch = useDispatch();
@@ -24,24 +26,38 @@ const PostsPage = () => {
 
   const page = Number(searchParams.get('page') || 1);
 
+  // const tags = useSelector(selectTags);
+
+  // useEffect(() => {
+  //   dispatch(getAllTags());
+  // }, []);
+
   useEffect(() => {
     dispatch(getAllPosts({ page }));
   }, [dispatch, page]);
 
   return (
     <>
-      <section className='flex flex-col items-center justify-between p-5 min-h-[85vh]'>
-        <div></div>
-        {isLoading ? (
-          <div className='text-[var(--color-text)]'>...Loading</div>
-        ) : posts?.length > 0 ? (
-          <div className='w-full flex flex-col gap-6 md:basis-4/5'>
-            <PostsList />
-          </div>
-        ) : (
-          <h2 className='m-auto'>Posts not found...</h2>
-        )}
-      </section>
+      <div className='block md:flex md:justify-between'>
+        <section className='flex flex-col p-5 md:w-1/5'>
+          <h2 className='hidden md:block mb-2 text-lg font-bold tracking-tight text-[var(--color-text)] text-center'>
+            Category
+          </h2>
+          <TagsBlock />
+        </section>
+        <section className='flex flex-col items-center justify-between p-5 min-h-[85vh]'>
+          <div></div>
+          {isLoading ? (
+            <div className='text-[var(--color-text)]'>...Loading</div>
+          ) : posts?.length > 0 ? (
+            <div className='w-full flex flex-col gap-6 md:basis-4/5'>
+              <PostsList />
+            </div>
+          ) : (
+            <h2 className='m-auto'>Posts not found...</h2>
+          )}
+        </section>
+      </div>
       {posts?.length !== 0 && (
         <section>
           <Pagination
