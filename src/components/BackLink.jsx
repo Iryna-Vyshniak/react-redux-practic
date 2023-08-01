9a1b94ed1c8733f80fe847d9eb7165a7f1883052
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-01 22:57:22 +0300

    update UI for back link

--- src/components/BackLink.jsx
+++ src/components/BackLink.jsx
@@ -5,7 +5,7 @@ export const BackLink = ({ to, children }) => {
   return (
     <Link
       to={to}
-      className='inline-flex items-center mb-4 py-2 px-1 text-blue-700 uppercase text-xs font-medium rounded-md shadow-lg transition duration-200 ease-in-out hover:bg-sky-50 hover:text-blue-400 hover:drop-shadow-md shadow-emerald-50'
+      className='inline-flex items-center px-3 py-2 w-20 h-11 text-sm font-medium text-center  text-gray-900 hover:text-white bg-[#f0fdf4] rounded-lg hover:bg-lime-700 focus:ring-2 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 shadow-xl hover:shadow-md'
     >
       <HiArrowLeft className='w-4 h-4 space-x-3' />
       {children}
