e43c5b2a861e5f55d5a81c2ba875861c20997cf0
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-02 02:14:33 +0300

    add selector for select all tags

--- src/store/posts/selectors.js
+++ src/store/posts/selectors.js
@@ -4,6 +4,7 @@ export const selectError = (state) => state.posts.error;
 export const selectPosts = (state) => state.posts.items;
 export const selectPostDetails = (state) => state.posts.postDetails;
 export const selectPopularPosts = (state) => state.posts.popularPosts;
+export const selectTags = (state) => state.posts.tags;
 
 export const selectCurrentPage = (state) => state.posts.currentPage;
 export const selectTotalPages = (state) => state.posts.totalPages;
