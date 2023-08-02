8286b797cc89921a3e5adbed277f851ba9f215c8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-03 00:28:16 +0300

    create and complete block with popular posts

---
+++ src/components/Posts/PopularPosts.jsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllPosts } from '../../store/posts/thunks';
+import { selectPopularPosts } from '../../store/posts/selectors';
+import { Link } from 'react-router-dom';
+
+export const PopularPosts = () => {
+  const dispatch = useDispatch();
+  const popularPosts = useSelector(selectPopularPosts);
+  // console.log(popularPosts);
+
+  const lastPopularPosts = popularPosts?.slice(0, 3);
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch]);
+
+  const shortenedTitles = (obj) => {
+    if (obj.title.length > 30) {
+      return obj.title.substring(0, 30) + '...';
+    } else {
+      return obj.title;
+    }
+  };
+
+  return (
+    <>
+      {lastPopularPosts?.length > 0 && (
+        <ul className='flex flex-wrap items-center justify-center space-x-2 md:flex-col md:space-x-0 w-full'>
+          {lastPopularPosts?.map((post) => (
+            <li key={post._id} className='mb-3 w-[120px]'>
+              <Link
+                to={{
+                  pathname: `/posts/${post._id}`,
+                  state: { from: location },
+                }}
+                className='flex flex-col items-center justify-center text-xs text-[var(--color-text)] text-center bg-[var(--button-color-active)] rounded-lg hover:bg-[var(--buttonHover)] focus:ring-2 focus:outline-none focus:ring-green-300 shadow-xl hover:shadow-md'
+              >
+                {post.imageUrl && (
+                  <img
+                    src={post.imageUrl}
+                    alt={post.title}
+                    className='w-full h-[5rem] object-cover rounded-t-lg'
+                    loading='lazy'
+                  />
+                )}
+                <div className='p-2'> {shortenedTitles(post)}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+};
