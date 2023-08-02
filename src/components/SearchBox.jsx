254406ba3efb841e3fa9ff78dc54fcb15cf986a3
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-03 00:32:18 +0300

    refactor search

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,15 +1,17 @@
 import { HiSearch } from 'react-icons/hi';
+import DebounceInput from 'react-debounce-input';
 
 export const SearchBox = ({ value, onChange }) => {
   return (
     <div className='flex items-center justify-center mb-4 mt-4 uppercase'>
       <div className='relative inline-flex items-center justify-center mb-4 mt-4 uppercase'>
         <HiSearch className='absolute right-1 w-5 h-5 text-red-700' />
-        <input
-          className='px-2 py-2 pr-8 rounded font-inherit shadow-lg shadow-red-100'
-          type='text'
+        <DebounceInput
+          className='px-2 py-2 pr-8 w-[60vw] rounded-2xl font-inherit shadow-lg outline-none border-none'
+          type='search'
           value={value}
           onChange={onChange}
+          debounceTimeout={1000}
         />
       </div>
     </div>
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,24 +1,33 @@
 import { NavLink, useLocation, useSearchParams } from 'react-router-dom';
-import { SearchBox } from '../components/Products/SearchBox';
-import { useEffect, useState } from 'react';
+import { SearchBox } from '../components/SearchBox';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectCurrentPage, selectProducts, selectTotalPages } from '../store/products/selectors';
+import {
+  selectCurrentPage,
+  selectIsLoading,
+  selectProducts,
+  selectTotalPages,
+} from '../store/products/selectors';
 import { ProductsList } from '../components/Products/ProductsList';
 import { Pagination } from '../components/Pagination';
-import { getProductsByQueryThunk } from '../store/products/thunks';
+import { getAllProductsThunk, getProductsByQueryThunk } from '../store/products/thunks';
+import { BackLink } from '../components/BackLink';
 
 const ProductPage = () => {
   // const [showForm, setShowForm] = useState();
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [searchParams, setSearchParams] = useSearchParams({ name: '', page: 1 });
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1, name: '' });
   // console.log(useSearchParams());
   //const { path } = history.location;
+  const location = useLocation();
 
-  let productName = searchParams.get('name') ?? '';
+  let name = searchParams.get('name') ?? '';
   const page = Number(searchParams.get('page') || 1);
   // console.log('PAGE: ', page, typeof page);
+  const BackLinkHref = location.state?.from ?? '/products';
   const currentPage = useSelector(selectCurrentPage);
   const totalPages = useSelector(selectTotalPages);
+  const products = useSelector(selectProducts);
+  const isLoading = useSelector(selectIsLoading);
 
   const dispatch = useDispatch();
 
@@ -26,24 +35,14 @@ const ProductPage = () => {
   //   setShowForm((prevState) => !prevState);
   // };
 
-  const location = useLocation();
-  const products = useSelector(selectProducts);
-
   useEffect(() => {
-    dispatch(getProductsByQueryThunk(productName));
-  }, [dispatch, productName]);
-
-  useEffect(() => {
-    const visibleProducts = products?.filter((product) =>
-      product.name.toLowerCase().includes(productName.toLowerCase())
-    );
-    setFilteredProducts(visibleProducts);
-  }, [productName, products]);
+    name !== '' ? dispatch(getProductsByQueryThunk(name)) : dispatch(getAllProductsThunk(page));
+  }, [dispatch, page, name]);
 
   const handleSearchChange = ({ target }) => {
     const inputValue = target.value;
-    productName = inputValue;
-    setSearchParams({ page: 1, name: productName });
+    name = inputValue;
+    setSearchParams({ page, name });
   };
 
   // const updateQueryString = (name) => {
@@ -55,7 +54,7 @@ const ProductPage = () => {
     <div className='flex flex-col items-center justify-between min-h-[85vh]'>
       <section className='flex flex-col items-center space-x-3 flex-1'>
         <div className='flex items-center space-x-3'>
-          <SearchBox value={productName} onChange={handleSearchChange} />
+          <SearchBox value={name} onChange={handleSearchChange} />
 
           <NavLink to={'add-product'} state={location.state}>
             Add product
@@ -66,8 +65,18 @@ const ProductPage = () => {
             {showForm ? 'Hide form' : 'Add product'}
           </NavLink> */}
         </div>
-
-        <ProductsList products={filteredProducts} page={page} />
+        {isLoading ? (
+          <div className='text-[var(--color-text)]'>...Loading</div>
+        ) : products?.length > 0 ? (
+          <ProductsList />
+        ) : (
+          <>
+            <div className='mb-5 px-4'>
+              <BackLink to={BackLinkHref}>Products</BackLink>
+            </div>
+            <h2 className='m-auto'>Posts not found...</h2>
+          </>
+        )}
       </section>
       {products.length > 0 && (
         <section>
