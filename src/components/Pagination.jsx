e3a26c5ee861771a7cd5faac91ff625bbc73046c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-02 04:34:55 +0300

    update UI for active btn

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -22,9 +22,9 @@ export const Pagination = ({ setSearchParams, currentPage, totalPages }) => {
       })}
       containerClassName='flex justify-center gap-4 my-10 mb-6 text-sm'
       //   pageClassName='flex items-center justify-center border border-solid border-blue-50 hover:bg-red-400 hover:text-white w-10 h-10 rounded-md'
-      activeClassName='selected bg-[var(--pagination-btn)] rounded-full'
+      activeClassName='selected bg-[var(--active-pagination-btn)] rounded-full'
       pageLinkClassName='flex items-center justify-center p-2 w-8 h-8 rounded-full text-center border-solid border-2 border-white rounded-full outline-none text-[var(--color-text)] bg-transparent cursor-pointer shadow-md hover:scale-105 transition-scale duration-200 ease-in-out'
-      activeLinkClassName='border-solid border-2 border-white rounded-full bg-[var(--pagination-btn)]  text-accent hover:scale-105 transition-scale duration-200 ease-in-out'
+      activeLinkClassName='border-solid border-2 border-white rounded-full bg-[var(--active-pagination-btn)]  text-accent hover:scale-105 transition-scale duration-200 ease-in-out'
       previousClassName='flex items-center justify-center p-2 w-8 h-8 rounded-full text-[var(--color-text)] text-center border-solid border-2 border-white rounded-full outline-none shadow-md bg-transparent cursor-pointer'
       nextClassName='flex items-center justify-center p-2 w-8 h-8 rounded-full text-[var(--color-text)] text-center border-solid border-2 border-white rounded-full outline-none  bg-transparent cursor-pointer shadow-md'
       breakClassName='flex items-center justify-center p-2 w-8 h-8 rounded-full text-center border-solid border-2 border-white rounded-full outline-none  text-[var(--color-text)] bg-transparent cursor-pointer'
