8b7770d6a5d79cc552be42b3a697ad76d6244643
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-19 16:22:34 +0300

    create and complete with start dynamic functionality component datapicker

---
+++ src/components/Calendar/CalendarDataPicker.jsx
@@ -0,0 +1,35 @@
+import { forwardRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import '../../../node_modules/react-datepicker/dist/react-datepicker.css';
+
+export default function CalendarDataPicker() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  // eslint-disable-next-line react/display-name
+  const CustomInput = forwardRef(({ value, onClick }, ref) => {
+    return (
+      <button
+        className='py-2 px-4 w-[144px] h-[44px] rounded-2xl text-white bg-blue-700 text-sm text-center uppercase font-bold'
+        onClick={onClick}
+        ref={ref}
+      >
+        {value}
+      </button>
+    );
+  });
+
+  console.log(startDate);
+
+  return (
+    <>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        customInput={<CustomInput />}
+        dateFormat='MMMM yyyy'
+        calendarStartDay={1}
+        formatWeekDay={(day) => day.substr(0, 1)}
+      />
+    </>
+  );
+}
